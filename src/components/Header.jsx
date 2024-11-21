b4b96c40b861eac3bbcac53fd3dd7b0190a7a038
Bilguun <Bilguun040728@gmail.com>
2024-11-21 13:24:26 +0800

    a

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,17 +4,17 @@ import Link from "next/link";
 import Image from "next/image";
 const Header = () => {
   return (
-    <header className=" z-10 fixed w-screen bg-[#2B4B8C] mx-auto py-6">
+    <header className=" z-10 fixed w-screen bg-[#2B4B8C] mx-auto py-6 px-[200px]">
       <nav className="flex items-center justify-between">
         <div className="flex items-center gap-2">
           <Image
-            src="/placeholder.svg"
-            alt="Zevtabs logo"
+            src="/logo.png"
+            alt="E-Account"
             width={40}
             height={40}
             className="h-10 w-10"
           />
-          <span className="text-2xl font-bold text-white">Zevtabs</span>
+          <span className="text-2xl font-bold text-white ">E-Account</span>
         </div>
 
         <div className="hidden md:flex items-center gap-8">
